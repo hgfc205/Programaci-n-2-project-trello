7aaf82c993f176ffa07c3f20b581ece6195c2051
Hektor Germain Flores Cabrera <hektorgermain@gmail.com>
2021-01-14 17:50:48 -0700

    Update trello.cpp

--- trello.cpp
+++ trello.cpp
@@ -1,43 +1,71 @@
 #include <iostream>
+#include <conio.h>
+
 //variables para otras cosas del programa
 int acc;
+char tareas[100,10], cont, cont2;
+
 //variables para los whiles xd
-char continuar;
+char continuar, continuar2, continuar3;
+
+//prototipos de funciones
+char uno(int contador);
 
 int main(){
 	continuar='s';
+	
 	while (continuar=='s'){
 		std::cout<<"bienvenido a trello "<<endl;
-		std::cout<<"---------------------------------"<<endl;
+		td::cout<<"\n---------------------------------\n";
 		std::cout<<"1- añadir una nueva tarea \n";
 		std::cout<<"2- ver progresión de la tareas \n";
 		std::cout<<"3- ver tareas completadas \n";
 		std::cout<<" ";
 		std::cout<<"¿que es lo que desea hacer? ";
 		std::cin>>acc;
-		std::cout<<"---------------------------------"<<endl;
+		std::cout<<"\n---------------------------------\n";
 		
-		while((acc>3) and (acc<1)){
+		while((acc!=3) and (acc!=1) and (acc!=2)){
 			system("cls");
 			std::cout<<"ha ingresado un digito erroneo\n";
-			return 0;
+			getch();
 			std::cout<<"bienvenido a trello "<<endl;
-			std::cout<<"---------------------------------"<<endl;
+			std::cout<<"\n---------------------------------\n";
 			std::cout<<"1- añadir una nueva tarea \n";
 			std::cout<<"2- ver progresión de la tareas \n";
 			std::cout<<"3- ver tareas completadas \n";
 			std::cout<<" ";
 			std::cout<<"¿que es lo que desea hacer? ";
 			std::cin>>acc;
-			std::cout<<"---------------------------------"<<endl;
+			std::cout<<"\n---------------------------------\n";
 		}
+		
+		system("cls");
+		continuar2='s';
 		
 		switch(acc){
 			case 1:	
+			while(continuar2=='s'){
+				uno(cont);
+				cont++;
+				continuar3='s';
+				while(continuar3=='s'){
+					
+					
+					std::cout<<"¿desea agregar otra sub tarea? (s = si y n = no)\n";
+					std::cin>>continuar3;
+				}
+				std::cout<<"¿desea agregar otra tarea nueva? (s = si y n = no)\n";
+				std::cin>>continuar2;
+			}
 			break;
+			
 			case 2:
+				
 			break;
+			
 			case 3:
+				
 			break;
 		}
 		
@@ -46,8 +74,12 @@ int main(){
 	return 0;
 }
 
-
-template <class vector>
-int uno(){
+//definiciones de funciones
+char uno(int contador){
+	char tarea;
 	
+	std::cout<<"ingrese su nueva tarea: ";
+	std::cin>>tarea;
+	tareas[cont,0]=tarea;
+	return 0;
 }
