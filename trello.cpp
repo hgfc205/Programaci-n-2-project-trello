8a1fe11da545a882b9f2a0aba18f82817e7b1cc8
Hektor Germain Flores Cabrera <72997155+hgfc205@users.noreply.github.com>
2021-01-26 17:26:08 -0700

    Update trello.cpp

--- trello.cpp
+++ trello.cpp
@@ -1,109 +1,97 @@
 #include <iostream>
 #include <conio.h>
+#include <string.h>
 
 //variables para otras cosas del programa
 int acc;
 char tareas[100][10];
 int cont, cont2;
 
-//variables para los whiles xd
+//variables para los ciclos xd
 char continuar, continuar2, continuar3;
+char comparar;
 
 //prototipos de funciones
-char uno(int contador);
-char subtarea(int contador2);
-
+void inicio();
+void uno(int contador);
+void subtarea(int contador2);
+void vertareas();
 
 int main(){
 	continuar='s';
 	
-	while (continuar=='s'){
-		std::cout<<"bienvenido a trello \n";
-		std::cout<<"\n---------------------------------\n";
-		std::cout<<"1- añadir una nueva tarea \n";
-		std::cout<<"2- ver progresión de la tareas \n";
-		std::cout<<"3- ver tareas completadas \n";
-		std::cout<<" ";
-		std::cout<<"¿que es lo que desea hacer? ";
-		std::cin>>acc;
-		std::cout<<"\n---------------------------------\n";
-		
-		while((acc!=3) and (acc!=1) and (acc!=2)){
-			system("cls");
-			std::cout<<"ha ingresado un digito erroneo\n";
-			getch();
-			std::cout<<"bienvenido a trello \n";
-			std::cout<<"\n---------------------------------\n";
-			std::cout<<"1- añadir una nueva tarea \n";
-			std::cout<<"2- ver progresión de la tareas \n";
-			std::cout<<"3- ver tareas completadas \n";
-			std::cout<<" ";
-			std::cout<<"¿que es lo que desea hacer? ";
-			std::cin>>acc;
-			std::cout<<"\n---------------------------------\n";
-		}
-		
+	while (continuar=='s')
+	{
 		system("cls");
-		continuar2='s';
+		inicio();
+		std::cin>>acc;
 		
-		switch(acc){
-			case 1:	
-			while(continuar2=='s'){
+		while((acc!=3) and (acc!=1) and (acc!=2))
+		{
+			system("cls");
+			std::cout<<"ha ingresado un digito erroneo \n";
+			std::cout<<"\n";
+			inicio();
+			std::cin>>acc;
+		}
+		system("cls");
+		
+		if (acc==1){
+			uno(cont);
+			cont++;
+			cont2=1;
+			
+			continuar2='s';
+			while (continuar2=='s'){
 				system("cls");
+				subtarea(cont2);
 				
-				uno(cont);
-				cont++;
-				cont2=1;
-				continuar3='s';
-				
-				while(continuar3=='s'){
-					system("cls");
-					
-					subtarea(cont2);
-					cont2++;
-					
-					std::cout<<"¿desea agregar otra sub tarea? (s = si y n = no)\n";
-					std::cin>>continuar3;
-					system("cls");
-				}
-				
-				std::cout<<"¿desea agregar otra tarea nueva? (s = si y n = no)\n";
+				cont2++;
+				std::cout<<"agregar otra sub tarea? ";
 				std::cin>>continuar2;
 			}
-			system("cls");
-			break;
+		} else if (acc==2){
 			
-			case 2:
-				
-			break;
-			
-			case 3:
-				
-			break;
-		} 
-		
-		system("cls");
+		}
 	}
 	return 0;
 }
 
 //definiciones de funciones
-char uno(int contador){
+void inicio(){
+	std::cout<<"bienvenido a trello \n";
+	std::cout<<"\n---------------------------------\n";
+	std::cout<<"1- añadir una nueva tarea \n";
+	std::cout<<"2- ver tareas \n";
+	std::cout<<"3- realizar progresión de una tarea \n";
+	std::cout<<"4- ver tareas completadas \n";
+	std::cout<<" \n";
+	std::cout<<"que es lo que desea hacer? ";
+}
+
+void uno(int contador){
 	char tarea;
 	
 	std::cout<<"ingrese su nueva tarea: ";
 	std::cin>>tarea;
 	tareas[cont][0]=tarea;
-	system("cls");
-	return 0;
 }
 
-char subtarea(int contador2){
+void subtarea(int contador2){
 	char sub_tarea;
 	
 	std::cout<<"ingrese la subtarea: ";
 	std::cin>>sub_tarea;
 	tareas[cont][cont2]=sub_tarea;
-	system("cls");
-	return 0;
 }
+
+void vertareas(){
+	for (int x=0; x<101; x++){
+		std::cout<<tareas[x][0];
+		for (int y=1; y<11; y++){
+			std::cout<<tareas[x][y];
+		}
+	}
+	getch();
+}
+
