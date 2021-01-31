f0a0d4052a42ebe16f6aff2ec04f10b923ae9333
Hektor Germain Flores Cabrera <72997155+hgfc205@users.noreply.github.com>
2021-01-30 22:12:31 -0700

    Update trello.cpp

--- trello.cpp
+++ trello.cpp
@@ -2,122 +2,78 @@
 #include <conio.h>
 #include <string.h>
 
-//variables para otras cosas del programa
-int acc;
-char tareas[99][10];
-int aux[100][2];
-int cont, cont2;
-
-//variables para los ciclos xd
-char continuar, continuar2;
+//variables
+int opcion, cont1=0, cont2;
+char tareas[20][10], tarea[20], subtarea[20];
 
 //prototipos de funciones
-void inicio();
-void uno(int contador);
-void subtarea(int contador1, int contador2);
-void vertareas();
-void progresionn_part_1();
+void menu();
+void agregartarea(int contador1, char tarea_agregada);
+void agregarsubtarea(int contador1, int contador2, char subtarea_agregada);
 
 int main(){
-	continuar='s';
+	int A=1;
+	int B=1;
 	
-	while (continuar=='s'){
+	while(A==1){
 		system("cls");
-		inicio();
-		std::cin>>acc;
+		menu();
+		std::cin>>opcion;
 		
-		while((acc!=3) and (acc!=1) and (acc!=2) and (acc!=4)){
+		if(opcion>3){
 			system("cls");
-			std::cout<<"ha ingresado un digito erroneo \n";
-			std::cout<<"\n";
-			inicio();
-			std::cin>>acc;
-		}
-		system("cls");
-		
-		switch(acc){
-			case 1:
-				uno(cont);
-				cont++;
-				
-				continuar2='s';
-				cont2=0;
-				
-				while(continuar2=='s'){
-					system("cls");
-					cont2++;
+			std::cout<<"Se ha equivocado de digito\n";
+			std::cout<<"Por favor pruebe a ingresar el digito correcto\n";
+			getch();
+		} else{
+			system("cls");
+			switch(opcion){
+				case 1:
+					std::cin.ignore();
+					std::cout<<"Ingrese su tarea: ";
+					std::cin.getline(tarea,20,'\n');
 					
-					if (cont2<=10){
-						subtarea(cont, cont2);
-						std::cout<<"\n";
-						std::cout<<"desea agregar otra sub tarea? ";
-						std::cin>>continuar2;
-					} else{
-						std::cout<<"el maximo de subtareas es 10";
-						cont2=10;
-						getch();
-						break;
+					agregartarea(cont1, tarea[20]);
+					
+					while((B!=2) and (cont2<=10)){
+						system("cls");
+						std::cout<<"Ingrese su subtarea: ";
+						std::cin.getline(subtarea,20,'\n');
+						agregarsubtarea(cont1,cont2,subtarea[20]);
+						std::cout<<"\n \n";
+						std::cout<<"Desea agregar otra subtarea? ";
+						std::cin>>B;
 					}
-				}
-				aux[cont][0]=cont2;
-			break;
+				break;
 				
-			case 2:
-				vertareas();
-			break;
-			
-			case 3:
-				
-			break;
+				case 2:
+					
+				break;
+			}
 		}
 	}
 	return 0;
 }
 
-//definiciones de funciones
-void inicio(){
-	std::cout<<"bienvenido a trello \n";
-	std::cout<<"\n---------------------------------\n";
-	std::cout<<"1- añadir una nueva tarea \n";
-	std::cout<<"2- ver lista de tareas \n";
-	std::cout<<"3- realizar progresión de una tarea \n";
-	std::cout<<"4- ver tareas completadas \n";
-	std::cout<<" \n";
-	std::cout<<"que es lo que desea hacer? ";
-}
-
-void uno(int contador){
-	char tarea[30];
-	
-	std::cout<<"ingrese su nueva tarea: ";
-	std::cin.getline(tarea,30,'\n');
-	tareas[cont][0]=tarea[30];
-}
-
-void subtarea(int contador1, int contador2){
-	char sub_tarea[30];
-	
-	std::cout<<"ingrese la subtarea: ";
-	std::cin.getline(sub_tarea,30,'\n');
-	tareas[cont][cont2]=sub_tarea[30];
-}
-
-void vertareas(){
-	std::cout<<"Lista de tareas: ";
-	for (int x=0; x<=cont; x++){
-		std::cout<<"\n"<<x+1<<"- "<<tareas[x][0]<<"\n\n";
-		for (int y=1; y<=aux[cont][0]; y++){
-			std::cout<<"  "<<x+1<<"."<<y<<" -> "<<tareas[x][y]<<"\n";
-		}
-	}
-	getch();
-}
-
-void progresionn_part_1(){
-	std::cout<<"realizar progresión de tarea\n";
+void menu(){
+	std::cout<<"Bienvenido "<<"\n";
+	std::cout<<"\n \n";
+	std::cout<<"OPCIONES A REALIZAR: \n";
+	std::cout<<"1-> Agregar una nueva tarea"<<"\n";
+	std::cout<<"2-> Ver lista de tareas"<<"\n";
+	std::cout<<"3-> Realizar un avance en una tarea"<<"\n";
 	std::cout<<"\n";
-	
-	for(int e=0; e<=cont; e++){
-		std::cout<<e<<"- "<<tareas[e][0];
-	}
+	std::cout<<"Digite su opcion a realizar: ";
 }
+
+void agregartarea(int contador1, char tarea_agregada){
+	tareas[contador1][0]=tarea_agregada;
+	cont1++;
+	cont2=1;
+}
+
+void agregarsubtarea(int contador1, int contador2, char subtarea_agregada){
+	tareas[contador1][contador2]=subtarea_agregada;
+	cont2++;
+}
+
